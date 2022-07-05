62b52750216a3db72054bc1cd70fdf73464e8604
Rahul jain <rahul.jain@w3villa.com>
2022-07-05 15:37:06 +0530

    final commit

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,58 @@
+class BlogsController < ApplicationController
+    
+    def index
+        @blogs = Blog.all
+    end
+
+    def new 
+        @blog = Blog.new
+    end
+
+    def show
+        @blog = Blog.find(params[:id])
+    end
+
+    def edit
+        @blog = Blog.find(params[:id])
+    end
+
+    def create
+        @blog = Blog.new(blog_params)
+            if @blog.save
+                redirect_to blogs_path
+            else
+                render  :new, :status, :unprocessable_entity  
+            end    
+    end 
+
+    def update
+        @blog = Blog.find(params[:id])
+        if @blog.update(blog_params)
+            redirect_to blogs_path
+        else
+          render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @blog = Blog.find(params[:id])
+        if @blog.destroy
+                redirect_to root_path
+            else
+                redirect_to root_path, status: :see_other
+            end
+    end
+
+
+    
+    private
+        def find_blog
+            @blog = Blog.find(params[:id])
+        end
+
+        def blog_params
+            params.require(:blog).permit(:title, :description, :category_id)
+        end
+
+
+end
